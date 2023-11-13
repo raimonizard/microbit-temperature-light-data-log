fcb21d3864881bf6329a19c17baa2e2e0a56d40d
raimonizard <97727989+raimonizard@users.noreply.github.com>
2023-11-13 16:52:30 +0100

    feat: add new variables to track

--- main.ts
+++ main.ts
@@ -1,23 +1,28 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    logging = !logging
+input.onButtonPressed(Button.A, function () {
+    logging = !(logging)
     if (logging) {
         basic.showIcon(IconNames.Target)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
     } else {
         basic.clearScreen()
     }
-    
 })
 let logging = false
 basic.clearScreen()
 logging = false
-loops.everyInterval(30000, function on_every_interval() {
+loops.everyInterval(30000, function () {
     if (logging) {
-        datalogger.log(datalogger.createCV("temp", input.temperature()), datalogger.createCV("light", input.lightLevel()))
+        datalogger.log(
+        datalogger.createCV("temp", input.temperature()),
+        datalogger.createCV("light", input.lightLevel()),
+        datalogger.createCV("sound-level", input.soundLevel()),
+        datalogger.createCV("acc-x", input.acceleration(Dimension.X)),
+        datalogger.createCV("acc-y", input.acceleration(Dimension.Y)),
+        datalogger.createCV("acc-z", input.acceleration(Dimension.Z)),
+        datalogger.createCV("compass-heading", input.compassHeading())
+        )
         basic.showIcon(IconNames.Yes)
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
     }
-    
     basic.showIcon(IconNames.Target)
 })
