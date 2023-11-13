507518736406649d328dd12fe412c5e1509437b3
raimonizard <97727989+raimonizard@users.noreply.github.com>
2023-11-13 17:27:28 +0100

    feat: major changes add sample-id funct. and fix bug with showing icons

--- main.ts
+++ main.ts
@@ -7,12 +7,24 @@ input.onButtonPressed(Button.A, function () {
         basic.clearScreen()
     }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("" + (sample))
+    if (logging) {
+        basic.showIcon(IconNames.Target)
+    } else {
+        basic.clearScreen()
+    }
+})
 let logging = false
+let sample = 0
 basic.clearScreen()
+sample = 0
 logging = false
+basic.showIcon(IconNames.Happy)
 loops.everyInterval(30000, function () {
     if (logging) {
         datalogger.log(
+        datalogger.createCV("sample-id", sample),
         datalogger.createCV("temp", input.temperature()),
         datalogger.createCV("light", input.lightLevel()),
         datalogger.createCV("sound-level", input.soundLevel()),
@@ -21,8 +33,21 @@ loops.everyInterval(30000, function () {
         datalogger.createCV("acc-z", input.acceleration(Dimension.Z)),
         datalogger.createCV("compass-heading", input.compassHeading())
         )
-        basic.showIcon(IconNames.Yes)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.UntilDone)
+        sample += 1
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.yawn), SoundExpressionPlayMode.InBackground)
+        for (let index = 0; index < 2; index++) {
+            basic.showIcon(IconNames.Yes)
+            basic.pause(200)
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                # # # # #
+                . . # . .
+                . . # . .
+                `)
+            basic.pause(200)
+        }
+        basic.clearScreen()
+        basic.showIcon(IconNames.Target)
     }
-    basic.showIcon(IconNames.Target)
 })
